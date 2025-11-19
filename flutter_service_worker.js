d8f6738c7d52198595441ae32419fd22f3cee64f
manojNeelam <manojneelam143@gmail.com>
2025-11-19 15:42:30 +0530

    Go2 pro fix v9

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,12 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "ec345209760dc403c32984b76f56cf21",
+const RESOURCES = {"flutter_bootstrap.js": "7ce031ae7f01688ca8ce1e1fe1b40ff5",
 "version.json": "a00566041bbc4ac690e7fe224bf11243",
 "index.html": "7f88f60b20e169ec5c51d78ae02c8c8e",
 "/": "7f88f60b20e169ec5c51d78ae02c8c8e",
 "firebase-messaging-sw.js": "06f7e4e7756a6fc260d15dcaaf9269ef",
-"main.dart.js": "65af1eaba8534bb4fc24148dfc6e6d60",
+"main.dart.js": "39b100a4ae310c31c6e9f53fa121c455",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "5a865aa7dd2be6d906c3b698140229fc",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
