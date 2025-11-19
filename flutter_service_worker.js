5c5b6fa4b99dcd7431f8d102a798e50797e7a473
manojNeelam <manojneelam143@gmail.com>
2025-11-19 08:08:40 +0530

    Fixed go2 v4

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "7defaba141371fe4b0813e4b019a2708",
+const RESOURCES = {"flutter_bootstrap.js": "66b6a38159c4d25703be5c47eb975370",
 "version.json": "a00566041bbc4ac690e7fe224bf11243",
 "index.html": "7f88f60b20e169ec5c51d78ae02c8c8e",
 "/": "7f88f60b20e169ec5c51d78ae02c8c8e",
